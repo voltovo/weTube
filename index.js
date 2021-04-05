4e31704eb4ca86b17a2b1049a6e39bf55dff04fc
suman choi <sumanchoi@sumanui-MacBookAir.local>
2021-04-06 06:31:09 +0900

    handling Express

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ const PORT = 4000;
 function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handleHome(req, res){
+    console.log(req);
+    res.send("Hello from Home");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
