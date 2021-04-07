6ffa22f28a4a297bb4826c8127706b5c8176452a
suman choi <sumanchoi@sumanui-MacBookAir.local>
2021-04-08 06:10:23 +0900

    cookieParser, bodyParser, helmet add

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express()
 const PORT = 4000;
 
@@ -8,11 +13,11 @@ const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req,res,next) =>{
-    console.log("Between");
-    next();
-}
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet);
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-app.listen(PORT, handleListening); 
+app.listen(PORT, handleListening);
