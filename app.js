467181d8ec5073d7c07ca7707c1baa4c3e9ba29d
sumanChoi <voltovo@naver.com>
2021-04-09 16:34:27 +0900

    feat:userRouter apply

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import {userRouter} from "./router";
 
 const app = express()
 
@@ -17,5 +18,6 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+app.use("/user", userRouter);
 
 export default app;
