dab1ce6b7a6e9e919391a1bf3e152f986eec79af
sumanChoi <voltovo@naver.com>
2021-04-09 16:59:55 +0900

    feat:MVC pattern apply for router

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express()
-
-const handleHome = (req, res) => res.send("Hello from Home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
+
 
 export default app;
