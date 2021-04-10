a10a7aa2903adfec4c2f490e8a7df2c0a0ab11c6
sumanChoi <voltovo@naver.com>
2021-04-10 09:22:36 +0900

    global routes path arrangement

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express()
 
@@ -15,9 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/users", userRouter);
-app.use("/videos", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 
 export default app;
