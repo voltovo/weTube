bb93bf29e8b0c7cc954bdbe59206ea34ade94bc5
sumanChoi <voltovo@naver.com>
2021-04-10 09:41:55 +0900

    global routes path arrangemnt 2

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+export const Join = (req, res) => res.send("Join");
+export const Login = (req, res) => res.send("Login");
+export const Logout = (req, res) => res.send("Logout");
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
+import { Join, Login, Logout } from "../controllers/userController";
+import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.send("Home"));
-globalRouter.get(routes.join, (req, res) => res.send("Join"));
-globalRouter.get(routes.login, (req, res) => res.send("Login"));
-globalRouter.get(routes.logout, (req, res) => res.send("Logout"));
-globalRouter.get(routes.search, (req, res) => res.send("Search"));
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, Join);
+globalRouter.get(routes.login, Login);
+globalRouter.get(routes.logout, Logout);
 
 export default globalRouter;
