170d2e3b7d1c2317c1c6eca23fadb6a2c2b69236
sumanChoi <voltovo@naver.com>
2021-04-13 06:10:08 +0900

    feat: search Controller modify

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,10 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+};
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
