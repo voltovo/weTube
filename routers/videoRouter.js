3e02a3c3f01660fb135dfd630e6958dda9fb24b1
sumanChoi <voltovo@naver.com>
2021-04-13 20:43:40 +0900

    feat: home page add video title,description

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { deleteVideo, editVideo, upload, videoDetail, videos } from "../controllers/videoController";
+import {
+  deleteVideo,
+  editVideo,
+  upload,
+  videoDetail,
+} from "../controllers/videoController";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
