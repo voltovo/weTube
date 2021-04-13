dabb8e011b907a1c414ece21f8ed01be9a077ef6
sumanChoi <voltovo@naver.com>
2021-04-13 17:08:05 +0900

    feat:edit Profile page add

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
+import {
+  changePassword,
+  editProfile,
+  userDetail,
+  users,
+} from "../controllers/userController";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
