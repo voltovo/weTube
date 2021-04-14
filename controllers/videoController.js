cadb2f8c43a599b730cc0d381a0b500fe83ba0a4
sumanChoi <voltovo@naver.com>
2021-04-14 16:29:51 +0900

    feat: search page in videos list

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -5,7 +5,11 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+  res.render("search", {
+    pageTitle: "Search",
+    searchingBy: searchingBy,
+    videos,
+  });
 };
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
