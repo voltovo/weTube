3711a3c6360ec0ed2d13353d2fd19283a730f4b8
sumanChoi <voltovo@naver.com>
2021-04-14 10:58:01 +0900

    feat: home controller in video

--- app.js
+++ app.js
@@ -9,20 +9,26 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
 
-const app = express()
+const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
 app.use(localsMiddleware);
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://archive.org"
+  );
+  return next();
+});
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
-
 
 export default app;
