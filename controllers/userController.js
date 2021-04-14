2dbead33c0b0b7c235ff905aa4081b2344df9a61
sumanChoi <voltovo@naver.com>
2021-04-14 16:57:03 +0900

    feat: login function add

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,11 @@ export const postJoin = (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
