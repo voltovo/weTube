6beb6354e75e528fc45ec7e5f209ee026d46a81a
sumanChoi <voltovo@naver.com>
2021-04-15 05:52:38 +0900

    feat:upload function add

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
 import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
 export const search = (req, res) => {
@@ -11,8 +13,16 @@ export const search = (req, res) => {
     videos,
   });
 };
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+
+  res.redirect(routes.videoDetail());
+};
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
