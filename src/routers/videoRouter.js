8e0f643c3403d901ccd7a46dadd7275d0211e437
sumanChoi <voltovo@naver.com>
2021-04-21 16:54:43 +0900

    feat:editVideo Page modify

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,20 +1,26 @@
 import express from "express";
 import {
   deleteVideo,
-  editVideo,
   postUpload,
   getUpload,
   videoDetail,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoController";
 import { uploadVideo } from "../../middlewares";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
+//upload Video
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+//video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+//edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+//delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
