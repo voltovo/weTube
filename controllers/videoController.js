917715850b34fe070f05d191ce4274521cb8c7be
sumanChoi <voltovo@naver.com>
2021-05-20 17:23:20 +0900

    feat:Upload user name add in vidoeDetail page

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,7 +41,10 @@ export const postUpload = async (req, res) => {
     fileUrl: path,
     title,
     description,
+    creator: req.user.id,
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 export const videoDetail = async (req, res) => {
@@ -49,7 +52,7 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
     res.render(`videoDetail`, { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
