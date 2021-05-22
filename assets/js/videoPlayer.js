1324cc1cf2fe16c73ad5b467e9e19b8461b05b8d
sumanChoi <voltovo@naver.com>
2021-05-23 08:01:11 +0900

    feat:video Screen full and exitFull function apply in videoPlayer page

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumBtn = document.getElementById("jsVolumBtn");
+const fullscrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -23,9 +24,23 @@ function handleVolumeClick() {
   }
 }
 
+function exitFullScreen() {
+  fullscrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullscrnBtn.addEventListener("click", goFullScreen);
+  document.exitFullscreen().catch((err) => Promise.resolve(err));
+}
+
+function goFullScreen() {
+  videoContainer.requestFullscreen();
+  fullscrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullscrnBtn.removeEventListener("click", goFullScreen);
+  fullscrnBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumBtn.addEventListener("click", handleVolumeClick);
+  fullscrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
