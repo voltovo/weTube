998fdd7d4b0d08b8328cd254e5a91ea0fb713081
sumanChoi <voltovo@naver.com>
2021-06-03 16:56:06 +0900

    feat:respons practice

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.end();
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}🚀`);
 app.listen(PORT, handleListening);
