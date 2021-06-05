6319a92bfcd584cabce8cdaf73590be584b77655
sumanChoi <voltovo@naver.com>
2021-06-05 12:15:14 +0900

    feat:apply routers one

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# WeTube
+# Wetube Reloaded
 
-Cloning Youtube with Vanilla and NodeJS
-
-## Pages
-
-- [ ] Home
-- [x] Join
-- [x] Login
-- [x] Search
-- [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [x] Upload
-- [x] Video Detail
-- [x] Edit Video
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+/users/edit -> Edit user
+/users/delete -> Delete user
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A
--- src/server.js
+++ src/server.js
@@ -5,12 +5,21 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.end();
-};
-
 app.use(logger);
-app.get("/", logger, handleHome);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("User");
+userRouter.get("/edit", handleEditUser);
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}🚀`);
