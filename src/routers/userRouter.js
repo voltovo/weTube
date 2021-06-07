b78fe2517227082be6d8610cfca3a58d8d8404dd
sumanChoi <voltovo@naver.com>
2021-06-07 19:49:19 +0900

    feat:planning routers apply

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { remove, edit } from "../controllers/userController";
+import { remove, edit, see, logout } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
-
+userRouter.get("/:id", see);
+userRouter.get("/logout", logout);
 export default userRouter;
