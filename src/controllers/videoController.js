2201e99e791b0a6d71df5d5e4614ae89c8b6e16c
sumanChoi <voltovo@naver.com>
2021-06-08 17:17:19 +0900

    feat:create partials folder and footer apply

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const edit = (req, res) => res.send("Edit Video");
-export const see = (req, res) => res.send("See Video");
-export const trending = (req, res) => res.send("Home Page Videos");
+export const see = (req, res) => res.render("watch");
+export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
