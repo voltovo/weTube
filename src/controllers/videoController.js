f500cc0ca3c6c5a18ae44989743236a7049fe2af
sumanChoi <voltovo@naver.com>
2021-06-09 15:42:20 +0900

    fix:pageTitle apply in edit and watch pages

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
-export const edit = (req, res) => res.send("Edit Video");
-export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.send("Edit Video", { pageTitle: "Edit" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
