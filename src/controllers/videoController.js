f04cb86e601de9a56a1ade6cf7d073286b13642f
sumanChoi <voltovo@naver.com>
2021-06-09 23:10:59 +0900

    feat:conditionals apply in base page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
+const fakeUser = {
+  userName: "suman",
+  loggedIn: true,
+};
+
 export const edit = (req, res) => res.send("Edit Video", { pageTitle: "Edit" });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
