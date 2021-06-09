1f2c39d77c0349080e88cf3d784cfcafaeae5a9a
sumanChoi <voltovo@naver.com>
2021-06-09 23:23:10 +0900

    feat:lteration apply in home page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,8 +5,11 @@ const fakeUser = {
 
 export const edit = (req, res) => res.send("Edit Video", { pageTitle: "Edit" });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8];
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
+
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
