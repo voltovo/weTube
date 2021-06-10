d1c4fd32b56973d84180f212cd04df1fb014e3ee
sumanChoi <voltovo@naver.com>
2021-06-10 22:54:53 +0900

    feat:array apply in watch page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,37 @@
-const fakeUser = {
-  userName: "suman",
-  loggedIn: true,
-};
-
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    creatAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    creatAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    creatAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 export const edit = (req, res) => res.send("Edit Video", { pageTitle: "Edit" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      creatAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      creatAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      creatAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const search = (req, res) => res.send("Search Video");
