59f4c4df1f3dc6062a50879bd75a8a029336c5a1
sumanChoi <voltovo@naver.com>
2021-06-10 17:10:16 +0900

    feat:mixin apply for video list

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,7 +6,32 @@ const fakeUser = {
 export const edit = (req, res) => res.send("Edit Video", { pageTitle: "Edit" });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8];
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      creatAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      creatAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      creatAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
 
