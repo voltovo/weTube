554078f06ffb386f1b379bbd1dd37632a28d72ee
sumanChoi <voltovo@naver.com>
2021-06-19 17:15:27 +0900

    feat:video model create complete

---
+++ src/models/Video.js
@@ -0,0 +1,15 @@
+import mongoose, { mongo } from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  createAt: Date,
+  hashtags: [{ type: String }],
+  meta: {
+    views: Number,
+    rating: Number,
+  },
+});
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
