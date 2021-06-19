2a56fc3ddcb1a76738fb916f2adf8cdbe5350d3a
sumanChoi <voltovo@naver.com>
2021-06-19 17:34:32 +0900

    feat:video model import in videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,49 +1,24 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    creatAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
+
+export const home = (req, res) => {
+  Video.find();
+  return res.render("home", { pageTitle: "Home" });
+};
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing:` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
-};
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+  return res.render("watch", { pageTitle: `Watching:` });
 };
 
 export const search = (req, res) => res.send("Search Video");
@@ -55,14 +30,5 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
   // here we will add a video to the videos array.
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    creatAt: "2 minutes ago",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
