b0ea88d2a919670b330adea71a8febd22b64f074
sumanChoi <voltovo@naver.com>
2021-06-22 22:24:14 +0900

    feat:video detail page modify

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,6 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
-    console.log(videos);
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -21,9 +20,10 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching:` });
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const search = (req, res) => res.send("Search Video");
