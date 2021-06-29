53d07f9ce82f5cffe1f1b574c30504c5b86eab87
sumanChoi <voltovo@naver.com>
2021-06-29 21:43:06 +0900

    feat:create login page

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { getJoin, login, postJoin } from "../controllers/userController";
+import {
+  getJoin,
+  getLogin,
+  postLogin,
+  postJoin,
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 export default rootRouter;
