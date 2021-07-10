e31fe6dbd3ee19f4d24893d5b87fb6cf4f33f212
sumanChoi <voltovo@naver.com>
2021-07-10 15:03:20 +0900

    feat:user access url setting use middleware

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,13 +7,14 @@ import {
   getEdit,
   postEdit,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/logout", logout);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
