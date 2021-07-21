59bff84ca9eb020f317f8595b26613f011deb23e
sumanChoi <voltovo@naver.com>
2021-07-21 19:49:44 +0900

    feat:video play and pause function apply

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,23 @@
-console.log("videoPlayer");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handleMute = (e) => {};
+
+const handlePause = (e) => (playBtn.innerHTML = "Play");
+const handlePlay = (e) => (playBtn.innerHTML = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
