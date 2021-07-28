0dd95d8c95a37f26ee0f1ac7b0b4c7cc1c750064
sumanChoi <voltovo@naver.com>
2021-07-28 20:58:44 +0900

    fea:video recoder start apply

--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -1,13 +1,42 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recoding";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recoding";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recoder = new MediaRecorder(stream);
+  recoder.ondataavailable = (e) => {
+    console.log("recoding done");
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log(recoder);
+  recoder.start();
+  console.log(recoder);
+  setTimeout(() => {
+    recoder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
