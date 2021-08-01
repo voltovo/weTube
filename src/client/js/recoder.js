a85af65dcee77998d9f58cac6fef2ff56419ba6d
sumanChoi <voltovo@naver.com>
2021-08-01 13:48:17 +0900

    feat:Downloading the video File

--- src/client/js/recoder.js
+++ src/client/js/recoder.js
@@ -3,8 +3,15 @@ const video = document.getElementById("preview");
 
 let stream;
 let recoder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "My Recording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = "Download Recoding";
@@ -19,9 +26,9 @@ const handleStart = () => {
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
 
-  recoder = new MediaRecorder(stream);
+  recoder = new MediaRecorder(stream, { MimeType: "video/webm" });
   recoder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
