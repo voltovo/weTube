a13a1ccd47c8ac607b25b85112754f31bdf609eb
sumanChoi <voltovo@naver.com>
2021-08-02 20:41:44 +0900

    feat:API Route part One apply

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,11 +1,18 @@
+import fetch from "node-fetch";
+
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: { text },
+  });
 };
 
-btn.addEventListener("click", handleSubmit);
+if (form) {
+  btn.addEventListener("click", handleSubmit);
+}
