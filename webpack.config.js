15500e9a20c41ded8d9d8091025f61d752ca5d10
sumanChoi <voltovo@naver.com>
2021-09-05 09:11:50 +0900

    feat:building the frontend

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,6 @@ module.exports = {
     recoder: BASE_JS + "recoder.js",
     commentSection: BASE_JS + "commentSection.js",
   },
-  mode: "development",
-  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
