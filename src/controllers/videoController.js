9b835cda2af38bddab95b7674298cb91342ad9cb
sumanChoi <voltovo@naver.com>
2021-09-13 21:54:35 +0900

    fixed:video file path modify

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -90,8 +90,8 @@ export const postUpload = async (req, res) => {
   try {
     const newVideo = await Video.create({
       title,
-      fileUrl: video[0].path,
-      thumbUrl: thumb[0].path,
+      fileUrl: video[0].location,
+      thumbUrl: thumb[0].location,
       description,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
