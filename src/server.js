bce8d44c0694d1b8f7e48987c37acb3d94c3c39e
sumanChoi <voltovo@naver.com>
2022-10-02 18:32:03 +0900

    FEAT:app.use method study

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ import express from "express";
 const PORT = "4000";
 const app = express();
 
-const middleWare = (req, res, next) => {
-	console.log(`Someone is going to : ${req.url}`);
+const logger = (req, res, next) => {
+	console.log(`${req.method} : ${req.url}`);
+	next();
+};
+
+const privateMiddleware = (req, res, next) => {
+	const url = req.url;
+	if (url === "/protected") res.send("<h1>NOT ALLOWED</h1>");
+
 	next();
 };
 
@@ -12,7 +19,9 @@ const handleHome = (req, res) => {
 	return res.send(`Someone is going to : ${req.url}`);
 };
 
-app.get("/", middleWare, handleHome);
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
 
 const handleListening = () =>
 	console.log(`✅ Server listenting on port ${PORT} 🚀`);
