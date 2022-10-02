75087e06c7234b88a0031a057bbbb0a193c57e10
sumanChoi <voltovo@naver.com>
2022-10-02 17:37:15 +0900

    FEAT:server req, res study

--- src/server.js
+++ src/server.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const PORT = "4000";
 const app = express();
+
+const handleHome = (req, res) => {
+	return res.send("I still hungry !");
+};
+
+const handleLogin = (req, res) => {
+	return res.send("Login here !");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
 	console.log(`✅ Server listenting on port ${PORT} 🚀`);
 
