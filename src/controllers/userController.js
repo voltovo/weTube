15180ef692b12db7330d9aa75601ba41131c564d
sumanChoi <voltovo@naver.com>
2022-10-13 20:56:47 +0900

    FEAT:router add video and user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,6 @@
 export const join = (req, res) => res.send("Join");
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send("Remove user");
+export const login = (req, res) => res.send("log In");
+export const logout = (req, res) => res.send("log Out");
+export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { edit, remove } from "../controllers/userController";
+import { edit, remov, see, logout } from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/:id", see);
+userRouter.get("/logout", logout);
 
 export default userRouter;
