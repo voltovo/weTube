d840bc93e923d304a0033f8acbf85c2305106824
sumanChoi <voltovo@naver.com>
2022-10-17 21:59:41 +0900

    FEAT: extending template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-	res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit Video");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => res.send("delete Video");
 export const upload = (req, res) => res.send("upload Video");
