a6b1f877e01078d1a8743a69d786aa2700cc9a90
sumanChoi <voltovo@naver.com>
2022-10-22 21:57:01 +0900

    FEAT:edit video apply

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,40 @@
 const videos = [
-    {
-        title : "First Video",
-        rating : 5,
-        comments : 2,
-        createdAt : "2 minutes ago",
-        views : 59,
-        id : 1
-    },
-    {
-        title : "second Video",
-        rating : 5,
-        comments : 2,
-        createdAt : "2 minutes ago",
-        views : 1,
-        id : 2
-    },
-    {
-        title : "Third Video",
-        rating : 5,
-        comments : 2,
-        createdAt : "2 minutes ago",
-        views : 59,
-        id : 3
-    },
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
 ];
 export const trending = (req, res) => {
-    res.render("home", { pageTitle: "Home", videos });
-}
-export const watch = (req, res) =>{
-    const {id} = req.params;
-    const video = videos[id - 1];
-    res.render("watch", { pageTitle: `Watching ${video.title}`, video});
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const search = (req, res) => res.send("Search Video");
-export const deleteVideo = (req, res) => res.send("delete Video");
-export const upload = (req, res) => res.send("upload Video");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching : ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+};
+export const postEdit = (req, res) => {};
