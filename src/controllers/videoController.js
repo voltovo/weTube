d60c4b216490bc85ec481afee2a72c1fb90179a2
sumanChoi <voltovo@naver.com>
2022-10-29 16:45:15 +0900

    FEAT:video model apply

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,21 @@
 import { add } from "nodemon/lib/rules";
+import Video from {", (error, videos. => {}./models/Video";
 
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {});
+  res.render("home", { pageTitle: "Home" });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watching : ${video.title}`, video });
+  res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+  res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -61,6 +34,5 @@ export const postUpload = (req, res) => {
     views: 59,
     id: videos.length + 1,
   };
-  videos.push(newVideo);
   return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import logger from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = "4000";
 const app = express();
 
 app.set("view engine", "pug");
@@ -17,7 +14,4 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/", globalRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on port ${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
