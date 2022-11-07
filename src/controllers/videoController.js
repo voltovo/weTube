e0728368ba638fdd4c4266b14b27e3880fd9643d
sumanChoi <voltovo@naver.com>
2022-11-08 06:48:16 +0900

    feat:video info hashtags update method

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,9 +25,19 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  video.title = title;
+  video.Description = description;
+  video.Hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startWith("#") ? word : `#${word}`));
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 
@@ -41,7 +51,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startWith("#") ? word : `#${word}`)),
       meta: {
         views: 0,
         rating: 0,
