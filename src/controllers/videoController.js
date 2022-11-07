809ace1dcd0b321a207d9423e07b609b3b6ccd45
sumanChoi <voltovo@naver.com>
2022-11-08 06:58:54 +0900

    feat:video info hashtags update method 2 with mongoose method

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -28,16 +28,17 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  video.title = title;
-  video.Description = description;
-  video.Hashtags = hashtags
-    .split(",")
-    .map((word) => (word.startWith("#") ? word : `#${word}`));
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
