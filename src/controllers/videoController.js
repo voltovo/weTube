7ae9c8c815017181b032530b77dcdee8e2e00cc1
sumanChoi <voltovo@naver.com>
2022-11-08 21:41:25 +0900

    feat:static method use with mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -35,9 +35,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -52,7 +50,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
       meta: {
         views: 0,
         rating: 0,
--- src/models/Video.js
+++ src/models/Video.js
@@ -11,9 +11,8 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
-videoSchema.pre("save", async function () {
-  console.log("save video info = " + this);
-  this.hashtags = this.hashtags[0]
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
