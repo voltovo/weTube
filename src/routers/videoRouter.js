c4bb539d52f2e220c78e8c4ad93fd74fcf4eb503
sumanChoi <voltovo@naver.com>
2022-11-08 21:53:26 +0900

    feat:delete video method generate

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,4 +13,5 @@ const videoRouter = express.Router();
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 export default videoRouter;
