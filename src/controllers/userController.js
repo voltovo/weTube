719b0fc8cfebc4d080cdd9ea4be1f88315522049
sumanChoi <voltovo@naver.com>
2022-11-10 07:09:32 +0900

    feat:Confirm that the username already exists in login from

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,10 +6,11 @@ export const getJoin = (req, res) => {
 export const postJoin = async (req, res) => {
   console.log(req.body);
   const { email, username, password, password2, name, locations } = req.body;
+  const pageTitle = "Join";
   // password가 일치한지 check
   if (password !== password2) {
     return res.status(400).render("join", {
-      pageTitle: "Join",
+      pageTitle,
       errorMessage: "Password confirmation deos not match.",
     });
   }
@@ -17,7 +18,7 @@ export const postJoin = async (req, res) => {
   const exist = await User.exists({ $or: [{ username }, { email }] });
   if (exist) {
     return res.status(400).render("join", {
-      pageTitle: "Join",
+      pageTitle,
       errorMessage: "This email/username is already taken.",
     });
   }
@@ -33,13 +34,28 @@ export const postJoin = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.render("join", {
-      pageTitle: "Join",
+      pageTitle,
       errorMessage: error._message,
     });
   }
 };
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send("Remove user");
-export const login = (req, res) => res.send("log In");
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+  const pageTitle = "Login";
+  const { username, password } = req.body;
+  const usernameExists = await User.exists({ username });
+  // username 이 존재하지 않는다, 미 가입 회원
+  if (!usernameExists) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  res.end();
+};
 export const logout = (req, res) => res.send("log Out");
 export const see = (req, res) => res.send("See User");
