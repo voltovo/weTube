9497f0a5a272a3241a083e3f7e26eeb5b2e03ac6
sumanChoi <voltovo@naver.com>
2022-11-11 06:59:07 +0900

    feat:session install

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import logger from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -10,8 +11,10 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "Hello!", resave: true, saveUninitialized: true }));
+
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-app.use("/", rootRouter);
 
 export default app;
