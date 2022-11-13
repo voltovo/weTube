0b4757fab1d9269fc10db65db1a1e9c87caf5957
sumanChoi <voltovo@naver.com>
2022-11-13 10:36:34 +0900

    feat:cookie expires setting about 24hours

--- src/server.js
+++ src/server.js
@@ -18,6 +18,9 @@ app.use(
     secret: "Hello!",
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 86400000,
+    },
     store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
