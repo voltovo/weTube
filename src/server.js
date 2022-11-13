0270fef2ef2612160a7bd85605b81622d94cf9aa
sumanChoi <voltovo@naver.com>
2022-11-13 12:11:59 +0900

    feat:cookie secret and mongodb_url write in env

--- src/server.js
+++ src/server.js
@@ -15,13 +15,13 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
       maxAge: 86400000,
     },
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 app.use((req, res, next) => {
