7d84e8330f6948deea19779fa0e0ad56eb55307d
sumanChoi <voltovo@naver.com>
2022-11-13 10:17:36 +0900

    feat:install mongoStore

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,7 +13,14 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: "Hello!", resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+  })
+);
 app.use((req, res, next) => {
   req.sessionStore.all((error, sessions) => {
     console.log(sessions);
