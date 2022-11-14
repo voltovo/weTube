ad157b4b40dd1c1b4f571405d45eb4c403d9175f
sumanChoi <voltovo@naver.com>
2022-11-15 07:30:12 +0900

    feat:github login api step three, access_token and get json data

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
@@ -83,8 +84,23 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
-  console.log(req.params);
+export const finishGithubLogin = async (req, res) => {
+  const accessBaseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GITHUB_CLIENTID,
+    client_secret: process.env.GITHUB_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const accessFinalUrl = `${accessBaseUrl}?${params}`;
+  const data = await fetch(accessFinalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log("githubLogin json ", json);
 };
 export const logout = (req, res) => res.send("log Out");
 export const see = (req, res) => res.send("See User");
