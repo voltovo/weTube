b599f2cf150e47cdb5e6e4e40b0103b8c8954e2d
sumanChoi <voltovo@naver.com>
2022-11-15 21:45:38 +0900

    feat:github login api step four, get user email info of github account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -103,14 +103,27 @@ export const finishGithubLogin = async (req, res) => {
   if ("access_token" in tokenRequest) {
     // access api
     const { access_token } = tokenRequest;
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         headers: {
           authorization: `Bearer ${access_token}`,
         },
       })
     ).json();
-    console.log("userRequest = ", userRequest);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: { authorization: `Bearer ${access_token}` },
+      })
+    ).json();
+    const email = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+
+    console.log("email = ", email);
+    if (!email) {
+      return res.redirect("/login");
+    }
   } else {
     return res.redirect("/login");
   }
