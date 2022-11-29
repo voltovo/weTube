b0323a9222ec60321bea53dac4d8e9d7b849fd7e
sumanChoi <voltovo@naver.com>
2022-11-29 21:06:21 +0900

    fix:Prohibit video data modification without permission

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -20,9 +20,15 @@ export const watch = async (req, res) => {
 };
 export const getEdit = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
   const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
   }
   return res.render("videos/edit", {
     pageTitle: `Edit : ${video.title}`,
@@ -33,8 +39,16 @@ export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
+  const videoData = await Video.findById(id);
+  const {
+    user: { _id },
+  } = req.session;
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  // 비디오 생성자 와 로그인 유저가 같은지 비교
+  if (String(videoData.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -83,6 +97,16 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
+  }
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
--- src/models/User.js
+++ src/models/User.js
@@ -18,7 +18,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  // 비밀 번호가 변경 되었을 때만 hash 처리
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
