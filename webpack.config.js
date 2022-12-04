a5fc3b4e2f818b1b52132a784c2158c507406bf2
sumanChoi <voltovo@naver.com>
2022-12-04 17:54:55 +0900

    feat:Set up auto start when changing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 module.exports = {
   entry: "./src/client/js/main.js",
+  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
@@ -11,6 +12,7 @@ module.exports = {
   output: {
     filename: "js/main.js",
     path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
