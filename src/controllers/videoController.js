67283bb82b097f63616ae4c85d8f4431f6e5a6ff
sumanChoi <voltovo@naver.com>
2022-12-06 22:04:47 +0900

    feat:scss style github login page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -121,7 +121,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(keyword, "i"),
       },
-    });
+    }).populate("owner");
   }
 
   return res.render("search", { pageTitle: "Search", videos });
