bfe80b02dcd5ffa8d82582226502e7e7b1193657
sumanChoi <voltovo@naver.com>
2022-12-09 23:08:12 +0900

    feat:video player currentTime setting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,8 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let globalVolume = 0.5;
 video.volume = globalVolume;
@@ -15,8 +17,10 @@ const handlePlayClick = (e) => {
   }
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
+
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Paused");
+
 const handleMute = (e) => {
   if (video.muted) {
     video.muted = false;
@@ -26,6 +30,7 @@ const handleMute = (e) => {
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
   volumeRange.value = video.muted ? 0 : globalVolume;
 };
+
 const handleVolumeChange = (event) => {
   const {
     target: { value },
@@ -38,6 +43,27 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const handleLoadedMetadata = () => {
+  const minutes = Math.floor(video.duration / 60);
+  let seconds = Math.floor(video.duration - minutes * 60);
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  totalTime.innerText = minutes + ":" + seconds;
+};
+
+const handleTimeUpdate = () => {
+  const videoPlayTime = Math.floor(video.currentTime);
+  let currentMinutes = Math.floor(videoPlayTime / 60);
+  let currentSeconds = videoPlayTime;
+  if (currentMinutes > 0) {
+    currentSeconds = currentSeconds - currentMinutes * 60;
+  }
+  currentMinutes = currentMinutes < 10 ? "0" + currentMinutes : currentMinutes;
+  currentSeconds = currentSeconds < 10 ? "0" + currentSeconds : currentSeconds;
+  currentTime.innerText = currentMinutes + ":" + currentSeconds;
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("change", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
