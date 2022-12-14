6749d4229bf7a8f46b120c86079000e5b9024e22
sumanChoi <voltovo@naver.com>
2022-12-14 15:30:49 +0900

    feat:video preview apply in upload page:

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,14 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: { width: { min: 1280 }, height: { min: 720 } },
   });
   console.log("stream = ", stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
