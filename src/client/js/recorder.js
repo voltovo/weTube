626f55c68e9e2a18f64515c4288f8abe881be178
sumanChoi <voltovo@naver.com>
2022-12-29 07:31:56 +0900

    feat:thumbnail file create

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -29,17 +29,34 @@ const handlDownload = async () => {
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
   // 만들어진 파일을 mp4파일로 변환
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "recording.mp4");
-
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
   const mp4File = ffmpeg.FS("readFile", "recording.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
   // binary data를 사용하기 위해 buffer
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "MyThumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 /** 비디오 녹화 종료 */
 const handleStop = () => {
