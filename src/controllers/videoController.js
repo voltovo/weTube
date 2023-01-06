948f9c5af650f0f4f6aad506fa9f4a842d6d1efe
sumanChoi <voltovo@naver.com>
2023-01-06 23:03:37 +0900

    feat:video thumbnail add

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -70,13 +70,14 @@ export const postUpload = async (req, res) => {
       user: { _id },
     },
     body: { title, description, hashtags },
-    file: { path },
+    files: { video, thumbnail },
   } = req;
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: path,
+      fileUrl: video[0].path,
+      thumbnailUrl: thumbnail[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
       meta: {
