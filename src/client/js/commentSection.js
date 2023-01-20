4e6d14aa760bc884d6cf82ff95dfad63152522ea
sumanChoi <voltovo@naver.com>
2023-01-20 18:51:24 +0900

    feat:add commentid

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,11 +1,12 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+const addComment = (text, commentId) => {
   const videoComments = document.querySelector(".video__comments ul");
 
   const newComment = document.createElement("li");
   newComment.className = "video__comment";
+  newComment.dataset.commentid = commentId;
 
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
@@ -13,8 +14,12 @@ const addComment = (text) => {
   const commentSpan = document.createElement("span");
   commentSpan.innerText = ` ${text}`;
 
+  const deleteSpan = document.createElement("span");
+  deleteSpan.innerText = " ❌";
+
   newComment.appendChild(icon);
   newComment.appendChild(commentSpan);
+  newComment.appendChild(deleteSpan);
 
   videoComments.prepend(newComment);
 };
@@ -28,7 +33,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     headers: {
       "Content-Type": "application/json",
     },
@@ -36,10 +41,10 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   });
 
-  textarea.value = "";
-  if (status === 201) {
-    console.log("create comments !");
-    addComment(text);
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+    textarea.value = "";
   }
 };
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -171,5 +171,5 @@ export const createComment = async (req, res) => {
   // 해당 비디오의 댓글 배열에 추가
   video.comments.push(comment._id);
   video.save();
-  return res.sendStatus(201);
+  return res.status(201).json({ newCommentId: comment._id });
 };
