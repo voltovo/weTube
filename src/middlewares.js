b4276dcf1eda6d3592e7a43a9f33aca1a1201481
sumanChoi <voltovo@naver.com>
2023-01-28 00:05:23 +0900

    feat:aws s3 multer add

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,24 @@
 const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+
+/*
+ * aws 자격 증명
+ * */
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+/*
+ * s3 업로드 multer
+ * */
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "sutube",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -28,8 +48,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars",
   limits: { filesize: 3000000 },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { filesize: 10000000 },
+  storage: multerUploader,
 });
